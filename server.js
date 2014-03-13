aed430e99d9441fed0ba60dff197033fa28f444f
Leslie Lin <leslie.s.lin@gmail.com>
2014-03-12 21:25:58 -0400

    code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 comsole.log('Hello');
-var server = http.createServer(function(req, res)){
+var server = http.createServer(function(req, res){
     res.writeHead(200, {'content-type'})
     res.end('<h1>HELLO KITTY WORLD</h1>');
 });
